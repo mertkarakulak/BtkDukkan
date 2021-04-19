1fe4798b445516bee2b3b71e74a81caefb49aa00
Mert Karakulak <my@mertkarakulak.com.tr>
2021-04-20 01:12:23 +0300

    backend

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            // iş kuralları
+            if (product.CategoryId == 1)
+            {
+                return new ErrorResult("Bu kategoriye ürün kabul edilmemektedir.");
+            }
+            else
+            {
+                _productDal.Add(product);
+                return new SuccessResult("Ürün Eklendi.");
+            }
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), "Ürünlerler listelendi.");
+        }
+
+        public IDataResult<List<Product>> GetAllBuyCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==categoryId), "Ürünlerler listelendi.");
+        }
+    }
+} 
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=BtkDukkan;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
